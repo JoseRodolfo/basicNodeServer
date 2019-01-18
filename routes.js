a3239520faf77db650e2a2ce7510f3669f72a14f
jose rodolfo perez <thejoserodolfo@gmail.com>
2019-01-17 23:15:56 -0800

    added a "users" route

--- routes.js
+++ routes.js
@@ -1,19 +1,27 @@
 // The URL module splits up a web address into readable parts.
 
+const routes = (req, res) => {
 
-const routes = (req,res) => {
-
-    // allows us to add 'sections' to our url
+  // allows us to add 'sections' to our url
   const url = req.url;
-    // to use POST GET methods etc
+  // to use POST GET methods etc
   const method = req.method;
 
-if(url === '/'){
-  res.write('<html>')
-  res.write('<head><title>Greeting</title></head>')
-  res.write('<body><h1>Yes, Your First Route! </h1></body>')
-  res.write('</html>')
-}
+  if (url === '/') {
+    res.write('<html>')
+    res.write('<head><title>Greeting</title></head>')
+    res.write('<body><h1>Yes, Your First Route! </h1></body>')
+    res.write('</html>')
+    return res.end()
+  }
+
+  if (url === '/users'){
+    res.write('<html>')
+    res.write('<head><title>Greeting</title></head>')
+    res.write('<body><ul><li>Jose</li><li>Josiah</li><li>Zoraida</li></ul></body>')
+    res.write('</html>')
+    return res.end()
+  }
 }
 
 module.exports = routes;
